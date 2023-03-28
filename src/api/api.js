a34a768dd40185547456a71a870f2568059f7cad
Bruno Novais <novais-bruno@hotmail.com>
2023-03-28 19:18:01 -0300

    QRCode generator

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:6000',
+    baseURL: 'https://qr-code-generation-api-7gq8.vercel.app/',
     timeout: 10000,
     headers: { 'Content-Type': 'application/json' }
 });
